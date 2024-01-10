ca2536775f334ab8c2fa26a8352d13bb7c93c0f7
SuvadeepHalder1 <suvadeephalder1@outlook.com>
2024-01-10 15:47:00 +0530

    add student.php

---
+++ Student.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+</title>BCA students</title>
+</head>
+<body>
+    <header><h1>Welcome BCA students</h1></header>
+    <main><p>Hi,my name is suvadeep halder</p></main>
+</body>
+</html>
